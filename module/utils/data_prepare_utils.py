fa6d84e3014177c69ce205ea792848ab874170b8
LiyangSong <song.liy@northeastern.edu>
2023-10-24 03:14:14 -0400

    add target encoding function

--- module/utils/data_prepare_utils.py
+++ module/utils/data_prepare_utils.py
@@ -1,4 +1,7 @@
+from IPython.core.display_functions import display
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import TargetEncoder
+import pandas as pd
 
 
 def sample_data(df, fraction, random_state=42):
@@ -30,3 +33,18 @@ def split_train_test_df(df, target, stratify=False, test_size=0.2, random_state=
         print(f"target distribution in test set: \n {test_y_df.value_counts(normalize=True)}")
 
     return train_cap_x_df, train_y_df, test_cap_x_df, test_y_df
+
+
+def target_encode_categorical(df, categorical_attr_list, target):
+    print("\nTarget encode categorical attributes:")
+
+    cap_x = df[categorical_attr_list]
+    y = df[target]
+    encoder = TargetEncoder(random_state=42)
+    cap_x_trans = encoder.fit_transform(cap_x, y)
+    cap_x_trans_df = pd.DataFrame(cap_x_trans, columns=cap_x.columns, index=df.index)
+
+    result = pd.concat([cap_x_trans_df, df.drop(columns=categorical_attr_list)], axis=1)
+    print(f"encoded_df.head():")
+    display(result.head())
+    return result
